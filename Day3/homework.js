f6bad967adb4b65f581c822106fbbb786a300e5e
Thai Vu <thaivu220802@gmail.com>
2020-09-01 23:54:57 +0700

    Fixed some typos in comment

--- Day3/homework.js
+++ Day3/homework.js
@@ -59,7 +59,7 @@ if (choice === "R") {
   lineup.splice(delIndex - 1, 1);
 }
 
-// 5.	Write a script to ask users enter a sequence of numbers, the Numbers are separated by commas, calculate the sum of the numbers and show it to users
+// 5.	Write a script to ask users enter a sequence of numbers, the numbers are separated by commas, calculate the sum of the numbers and show it to users
 
 const numSeq = prompt("Enter a sequence of numbers, seperated by commas: ");
 const numArr = numSeq.split(",");
