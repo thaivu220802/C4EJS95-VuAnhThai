d039154224a78657a6d7106906945de1c3f31d3a
Thai Vu <thaivu220802@gmail.com>
2020-09-01 20:01:46 +0700

    Add + Read

--- Day3/exercise.js
+++ Day3/exercise.js
@@ -1,3 +1,13 @@
 // Initialize an array named movies containing the titles of some of your favorite movies
 
 let movies = ["Parasite", "Your Name", "Weathering With You"];
+
+//Add new item named newMovie into the end of the movies array you initialized, newMovie entered by users
+
+let newMovie = prompt("Enter a movie that you like:");
+movies.push(newMovie);
+
+// Read the item at position i in the movies array, i entered by users
+
+let moviePosition = prompt("Enter a number between 1 to " + movies.length + ":");
+alert("The movie at the corresponding position is " + movies[moviePosition - 1] + ".");
