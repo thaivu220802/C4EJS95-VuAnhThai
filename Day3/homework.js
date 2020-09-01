ccf9ac1b9010a42b74234d85fb23d85da2caf87a
Thai Vu <thaivu220802@gmail.com>
2020-09-01 21:19:23 +0700

    Add tasks 5, 7 and 11

--- Day3/homework.js
+++ Day3/homework.js
@@ -7,15 +7,15 @@ let choice = prompt(
   "Hi there, welcome to shop admin panel, what do you want [C, R, U, D]? "
 ).toUpperCase();
 
-while (options.indexOf(choice)<0) {
-  choice = prompt("Invalid input. Please try again: ")
+while (options.indexOf(choice) < 0) {
+  choice = prompt("Invalid input. Please try again: ");
 }
 
 if (choice === "R") {
   console.log("The current items are:");
   for (let i = 0; i < lineup.length; i++) {
     console.log(i + 1 + ". " + lineup[i]);
-  };
+  }
 } else if (choice === "C") {
   const newItem = prompt("Enter the name of the new item: ");
   lineup.push(newItem);
@@ -26,4 +26,62 @@ if (choice === "R") {
 } else if (choice === "D") {
   const delIndex = prompt("Enter the position you want to delete");
   lineup.splice(delIndex - 1, 1);
-} 
+}
+
+// 5.	Write a script to ask users enter a sequence of numbers, the Numbers are separated by commas, calculate the sum of the numbers and show it to users
+
+const numSeq = prompt("Enter a sequence of numbers, seperated by commas: ");
+const numArr = numSeq.split(",");
+let sum = 0;
+
+for (let i = 0; i < numArr.length; i++) {
+  sum = numArr[i];
+}
+
+alert(`The sum of the numbers is ${sum}.`);
+
+// 6.	Write a script asking users to enter a sequence of numbers, the numbers are separated by commas, find the smallest number and log it out to users
+
+const numSeq = prompt("Enter a sequence of numbers, seperated by commas; ");
+const numArr = numSeq.split(",");
+let min = numArr[0];
+
+for (let i = 1; i < numArr.length; i++) {
+  if (numArr[i] < min) {
+    min = numArr[i];
+  }
+}
+
+alert(`The smallest number of the sequence is ${min}.`);
+
+//7.	Create an array containing at least 5 numbers, then ask users enter a number, perform a search to look for the number in the array, if the number is found, tell user that with the index of it in the array, if not, also tell them so
+
+const testArr = [3, 4, 6, -9, 10, -88, 2];
+const testNum = prompt("Enter a number: ");
+
+for (let i = 1; i < testArr.length; i++) {
+  if (testArr[i] === testNum) {
+    alert(`${testNum} is found in my array at index ${i}.`);
+    break;
+  } else {
+    alert(`${testNum} is not found in my array.`);
+  }
+}
+
+// 11. Write a script asking users to enter a sequence of Numbers, separated by commas (,). Create a new array containing only the odd Numbers of the entered sequence. If you need hints, they are at the end of this homework
+
+const numSeq = prompt("Enter a sequence of numbers: ");
+const numArr = numSeq.split(",");
+let numOdd = [];
+
+for (let i = 0; i < numArr.length; i++) {
+  if (numArr[i] % 2 === 1) {
+    numOdd.push(`${numArr[i]} `);
+  }
+}
+
+if (numOdd.length === 0) {
+  alert("Your sequence have no odd numbers.");
+} else {
+  alert(`The odd numbers of your sequence are: ${numOdd}.`);
+}
