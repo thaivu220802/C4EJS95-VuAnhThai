8d37f6bef4d4a9904b8ba19469f324208a8722ac
Thai Vu <thaivu220802@gmail.com>
2020-09-06 19:18:42 +0700

    Add index.html

--- Day4/objects.js
+++ Day4/objects.js
@@ -5,3 +5,11 @@ let movie = {
     year: 2012,
     rate: 8.4
 }
+
+// Log all of 3 the properties of the movie object to the console using 2 different ways
+
+console.log(movie);
+
+console.log(movie.title);
+console.log(movie.year);
+console.log(movie.rate);
