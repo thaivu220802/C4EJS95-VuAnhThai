5961915346e9d8b7fb596dcaea7d4f6b0c29042e
Thai Vu <thaivu220802@gmail.com>
2020-09-12 21:51:33 +0700

    Minor edits'

--- Day5-Lab/quiz.js
+++ Day5-Lab/quiz.js
@@ -2,42 +2,27 @@ export const QUIZ =
 [
   {
     "question": "Where did Novak Djokovic strike a US Open lineswoman with an errant ball?",
-    "choice1": "abdomen",
-    "choice2": "chest",
-    "choice3": "head",
-    "choice4": "throat",
+    "choices": ["abdomen", "chest", "head", "throat"],
     "rightChoice": "throat"
   },
   {
     "question": "Which state's Coalition parties threatened to split over koala protection laws?",
-    "choice1": "New South Wales",
-    "choice2": "South Australia",
-    "choice3": "Tasmania",
-    "choice4": "Western Australia",
+    "choices": ["New South Wales", "South Australia", "Tasmania", "Western Australia"],
     "rightChoice": "New South Wales"
   },
   {
     "question": "ABC journalist Bill Birtles fled which city on the advice of Australian Embassy staff?",
-    "choice1": "Beijing",
-    "choice2": "Hong Kong",
-    "choice3": "Shanghai",
-    "choice4": "Wuhan",
+    "choices": ["Beijing", "Hong Kong", "Shanghai", "Wuhan"],
     "rightChoice": "Beijing"
   },
   {
     "question": "Which company's internet banking systems suffered a six-hour outage on Monday?",
-    "choice1": "ANZ",
-    "choice2": "Commonwealth Bank",
-    "choice3": "NAB",
-    "choice4": "Westpac",
+    "choices": ["ANZ", "Commonwealth Bank", "NAB", "Westpac"],
     "rightChoice": "Westpac"
   },
   {
     "question": "Keeping Up with the Kardashians will come to an end in 2021 after how many years?",
-    "choice1": "6",
-    "choice2": "10",
-    "choice3": "14",
-    "choice4": "18",
+    "choices": ["6", "10", "14", "18"],
     "rightChoice": "14"
   }
 ]
