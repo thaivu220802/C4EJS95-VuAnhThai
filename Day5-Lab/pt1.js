30a657d05474772b577f4a33e96745dd71daadab
Thai Vu <thaivu220802@gmail.com>
2020-09-12 21:09:00 +0700

    Add lab problems

---
+++ Day5-Lab/pt1.js
@@ -0,0 +1,87 @@
+// Learn how to create a random number from 0 to 1
+
+let mathRand = Math.round(Math.random() * 100) / 100; // to 2 decimal places
+
+// Learn how to randomly pick an item from an array
+
+const testArr = [1, 3, 4, 5, 1, 3];
+
+function randomNum(start, stop) {
+  const diff = stop - start;
+  return Math.round(start + diff * Math.random());
+}
+
+function randomArr(arr) {
+  const length = arr.length;
+  return arr[randomNum(0, length)];
+}
+
+function shuffleArr(arr) {
+  let oldArr = arr;
+  let newArr = [];
+  while (oldArr.length > 1) {
+    let ind = randomNum(0, oldArr.length - 1);
+    newArr.push(oldArr[ind]);
+    oldArr.splice(ind, 1);
+  }
+  newArr.push(oldArr[0]);
+  return newArr;
+}
+
+console.log(shuffleArr(testArr));
+
+// Create a data structure to represent a list of quizzes, each quiz contains a question, 4 choices and rightChoice.
+
+import { QUIZ } from "./quiz.js";
+const QUIZ_LENGTH = QUIZ.length;
+const QUIZ_CHOICES = ["1", "2", "3", "4"];
+
+// Write a script to randomly select a quiz from the list above, show them to users
+// Let it run continuously, make sure each question only appears once, if the questions has run out, let users know
+// Calculate the score of the user
+// Shuffle the choices each time you show users the quiz
+
+let quizQuestionRem = [];
+for (let i = 0; i < QUIZ_LENGTH; i++) {
+  quizQuestionRem.push(i);
+}
+let score = 0;
+quizQuestionRem = shuffleArr(quizQuestionRem);
+
+for (let i = 0; i < QUIZ_LENGTH; i++) {
+  let quizRand = parseInt(quizQuestionRem[i]);
+  const quizQ = QUIZ[quizRand].question;
+  const quizA = QUIZ[quizRand].rightChoice;
+  let quizChoices = [QUIZ[quizRand].choice1, QUIZ[quizRand].choice2, QUIZ[quizRand].choice3, QUIZ[quizRand].choice4];
+  quizChoices = shuffleArr(quizChoices);
+  quizChoices.unshift(null);
+  let quizChoice = parseInt(prompt(`Question ${1 + i} out of ${QUIZ_LENGTH}: ${quizQ}
+1. ${quizChoices[1]}
+2. ${quizChoices[2]}
+3. ${quizChoices[3]}
+4. ${quizChoices[4]}
+Enter a number from 1 to 4 to select your answer:`));
+  while (QUIZ_CHOICES.indexOf(String(quizChoice)) < 0) {
+    quizChoice = prompt(`Invalid input. Please try again.
+Question ${1 + i} out of ${QUIZ_LENGTH}: ${quizQ}
+1. ${quizChoices[1]}
+2. ${quizChoices[2]}
+3. ${quizChoices[3]}
+4. ${quizChoices[4]}
+Enter a number from 1 to 4 to select your answer:`);
+  }
+  if (quizChoices[quizChoice] === quizA) {
+    score++;
+    alert(
+      `Bravo!!! Your score is now ${score}. ${QUIZ_LENGTH - i - 1} question(s) remain.`
+    );
+  } else {
+    alert(
+      `Wrong answer. Your score is still ${score}. ${QUIZ_LENGTH - i - 1} question(s) remain.`
+    );
+  }
+  console.log(quizQuestionRem);
+}
+
+alert(`We're out of questions! Your score is ${score}.`);
+//
---
+++ Day5-Lab/quiz.js
@@ -0,0 +1,44 @@
+export const QUIZ =
+[
+  {
+    "question": "Where did Novak Djokovic strike a US Open lineswoman with an errant ball?",
+    "choice1": "abdomen",
+    "choice2": "chest",
+    "choice3": "head",
+    "choice4": "throat",
+    "rightChoice": "throat"
+  },
+  {
+    "question": "Which state's Coalition parties threatened to split over koala protection laws?",
+    "choice1": "New South Wales",
+    "choice2": "South Australia",
+    "choice3": "Tasmania",
+    "choice4": "Western Australia",
+    "rightChoice": "New South Wales"
+  },
+  {
+    "question": "ABC journalist Bill Birtles fled which city on the advice of Australian Embassy staff?",
+    "choice1": "Beijing",
+    "choice2": "Hong Kong",
+    "choice3": "Shanghai",
+    "choice4": "Wuhan",
+    "rightChoice": "Beijing"
+  },
+  {
+    "question": "Which company's internet banking systems suffered a six-hour outage on Monday?",
+    "choice1": "ANZ",
+    "choice2": "Commonwealth Bank",
+    "choice3": "NAB",
+    "choice4": "Westpac",
+    "rightChoice": "Westpac"
+  },
+  {
+    "question": "Keeping Up with the Kardashians will come to an end in 2021 after how many years?",
+    "choice1": "6",
+    "choice2": "10",
+    "choice3": "14",
+    "choice4": "18",
+    "rightChoice": "14"
+  }
+]
+
