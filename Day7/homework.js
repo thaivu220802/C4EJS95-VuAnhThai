af2bb0135d7963d53131a7d015be8bb476392733
Thai Vu <thaivu220802@gmail.com>
2020-09-24 12:48:46 +0700

    Add exercises 1 4 7

--- Day7/homework.js
+++ Day7/homework.js
@@ -12,3 +12,30 @@ function yourWish(name, wish) {
     }
 }
 
+//1: 1d 2a 3c 4b
+
+// 4. Use getElementsByTagName function to get ALL of the <li> tag. Print/log out the first element (which is <li>Articles</li>) and then use for loop to print ALL of the DOM you get from the function.
+const elements = document.getElementsByTagName("LI");
+console.log(elements[0]);
+for (let i = 0; i < elements.length; i ++) {
+    console.log(elements[i].innerHTML);
+}
+
+// 5
+const singers = document.getElementsByTagName("div");
+console.log(singers[2]);
+for (let i = 1; i < singers.length; i ++) {
+    console.log(singers[i].innerHTML);
+}
+
+// 7
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+const input = document.getElementById("input");
+const buttons = [button1, button2];
+
+for (let button of buttons) {
+    button.addEventListener("click", (e) => console.log(e.target));
+}
+
+input.addEventListener("keydown", (e) => console.log(e.key))
