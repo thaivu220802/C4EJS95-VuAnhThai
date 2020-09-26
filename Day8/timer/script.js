82eda86ae579d3f1cccac745510858b9cf4dee3c
Thai Vu <thaivu220802@gmail.com>
2020-09-26 23:08:02 +0700

    Finish countdown timer

--- Day8/timer/script.js
+++ Day8/timer/script.js
@@ -2,11 +2,28 @@
 const timeInput = document.getElementById("timeInput")
 const startBtn = document.getElementById("startBtn")
 const stopBtn = document.getElementById("stopBtn")
+const timeDisplay = document.getElementById("timer")
+let countdownTimer;
 
 startBtn.onclick = () => {
     if (!/^\d+$/.test(timeInput.value)) {
         return;
     }
-    let timeLimit = parseInt(timeInput.value);
-    console.log(timeLimit);
+    let count = parseInt(timeInput.value);
+    timeDisplay.innerHTML = count;
+    countdownTimer = setInterval(timer, 1000)
+    function timer() {
+        count --;
+        if (count < 0) {
+            clearInterval(countdownTimer);
+            alert("Time's up!");
+            return;
+        }
+        timeDisplay.innerHTML = count;
+    }
+}
+
+stopBtn.onclick = () => {
+    clearInterval(countdownTimer);
+    timeDisplay.innerHTML = 0;
 }
