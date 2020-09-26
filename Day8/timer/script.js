561c8cf47548d12e267270b61aee4004f70f5da3
Thai Vu <thaivu220802@gmail.com>
2020-09-26 23:24:15 +0700

    Add sound and format timer

--- Day8/timer/script.js
+++ Day8/timer/script.js
@@ -3,6 +3,7 @@ const timeInput = document.getElementById("timeInput")
 const startBtn = document.getElementById("startBtn")
 const stopBtn = document.getElementById("stopBtn")
 const timeDisplay = document.getElementById("timer")
+const timeOutBeep = document.getElementById("timeOut")
 let countdownTimer;
 
 startBtn.onclick = () => {
@@ -10,16 +11,26 @@ startBtn.onclick = () => {
         return;
     }
     let count = parseInt(timeInput.value);
-    timeDisplay.innerHTML = count;
+    // timeDisplay.innerHTML = count;
     countdownTimer = setInterval(timer, 1000)
     function timer() {
-        count --;
         if (count < 0) {
             clearInterval(countdownTimer);
+            timeOutBeep.play();
             alert("Time's up!");
             return;
         }
-        timeDisplay.innerHTML = count;
+        let countMin;
+        let countSec;
+        if (count < 60) {
+            countMin = "";
+            countSec= count;
+        } else {
+            countMin = Math.floor(count / 60);
+            countSec = count % 60;
+        }
+        timeDisplay.innerHTML = `${countMin}:${('0' + countSec).slice(-2)}`;
+        count --;
     }
 }
 
