ff0ad3c3d7c60142191e0172cef5f2a86fc5fe3e
Thai Vu <thaivu220802@gmail.com>
2020-09-29 19:26:10 +0700

    Fix problems re. Delete button

--- Day7/Question_12/script.js
+++ Day7/Question_12/script.js
@@ -1,14 +1,5 @@
 // Initialize a variable named items to store the data above, log/print it out
 
-
-function remove(num) {
-  console.log("Remove");
-  console.log(`Index: ${num}`);
-  console.log(items);
-  let remover = document.getElementById(`${num}`);
-  remover.parentElement.remove();
-}
-
 let items = ["Backpack", "MiBand watch", "Ring"];
 
 // In HTML, create an <ul> with some test items.
@@ -16,10 +7,21 @@ let container = document.getElementById("list");
 for (let x of items) {
   container.insertAdjacentHTML(
     "beforeend",
-    `<li>${x} <button class="btn btn-danger onclick="remove(${items.indexOf(x)})" id="${items.indexOf(x)}">Remove</button> </li>`
+    `<li>${x} <button class="btn btn-danger" onclick="remove(${items.indexOf(
+      x
+    )})" id="${items.indexOf(x)}">Remove</button> </li>`
   );
 }
-let addIndex = items.length
+let addIndex = items.length;
+
+function remove(num) {
+  console.log("Remove");
+  console.log(`Index: ${num}`);
+  items[num] = "";
+  console.log(items);
+  let remover = document.getElementById(`${num}`);
+  remover.parentElement.remove();
+}
 
 // Get (Read) the list from DOM
 console.log(container);
@@ -32,12 +34,14 @@ let addButton = document.getElementById("add_btn");
 // When ‘Add’ button is clicked, the value of ‘New item’ input
 addButton.onclick = () => {
   console.log("Add button clicked");
-  console.log(newItem.value);
-  container.insertAdjacentHTML(
-    "beforeend",
-    `<li>${newItem.value} <button class="btn btn-danger" onclick="remove(${addIndex})" id="${addIndex}">Remove</button> </li>`
-  );
-  items.push(newItem.value);
-  addIndex ++;
-  newItem.value = "";
+  if (newItem.value.length > 0) {
+    console.log(newItem.value);
+    container.insertAdjacentHTML(
+      "beforeend",
+      `<li>${newItem.value} <button class="btn btn-danger" onclick="remove(${addIndex})" id="${addIndex}">Remove</button> </li>`
+    );
+    items.push(newItem.value);
+    addIndex++;
+    newItem.value = "";
+  }
 };
